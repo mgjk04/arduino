0dab79aac085a0b160b1eecafdc332c4db5549c1
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2026-02-10 15:18:42 +0800

    feat: abstract distance calculation

--- template/template.ino
+++ template/template.ino
@@ -4,6 +4,15 @@ int MOTOR_PIN1 = 6;
 int MOTOR_PIN2 = 5;
 float SPEED_OF_SOUND = 0.0345;
 
+float calculate_dist(int trig_pin, int echo_pin, int speed_of_sound) {
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  int microsecs = pulseIn(echo_pin, HIGH);
+  float dist_cm = microsecs * speed_of_sound / 2;
+  return dist_cm;
+}
+
 void setup() {
   pinMode(MOTOR_PIN1, OUTPUT);
   pinMode(MOTOR_PIN2, OUTPUT);
@@ -16,13 +25,9 @@ void setup() {
 
 void loop() {
   Serial.println("loop!"); // Debug IO statement
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  int microsecs = pulseIn(ECHO_PIN, HIGH);
-  float cms = microsecs * SPEED_OF_SOUND / 2;
-  Serial.println(cms);
-  if (cms < 30) {
+  int dist_cm = calculate_dist(TRIG_PIN, ECHO_PIN, SPEED_OF_SOUND);
+  Serial.println(dist_cm);
+  if (dist_cm < 30) {
     digitalWrite(MOTOR_PIN1, LOW);
     digitalWrite(MOTOR_PIN2, LOW);
   } else {
